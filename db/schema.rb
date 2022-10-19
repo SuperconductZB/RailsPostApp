976be444e404dde868ea13dc0708fbcc58fd697e
zephyrz73 <xzhou1551@outlook.com>
2022-10-18 20:20:11 -0700

    email field for user, destroy dependents, alert color to red

--- db/schema.rb
+++ db/schema.rb
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2022_10_19_003730) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
+    t.string "email"
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
